04dc5b5299f993832cb134934bf842bf3e744c4d
pburns_configureone <pburns@configureone.com>
2018-01-17 15:26:59 -0600

    Initial Commit

---
+++ fcc-calculator.js
@@ -0,0 +1,24 @@
+/*
+
+Free Code Camp - Build a JS Calculator
+Created by Patrick Burns (2017-01-17 - )
+
+TODO - Basic Calculator UI
+        -Buttons (0-9, ., plus, minus, mult, div, equal, clear)
+        -Text based Input, Current Expression, and Result Fields
+
+TODO - User Story: I can add, subtract, multiply and divide two numbers.
+        -Clickable buttons
+        -Mapping clicks to input, expression
+        -Current Input and Expression populate as I click numbers
+        -Result generated when I hit equal
+
+TODO - User Story: I can clear the input field with a clear button.
+        -Clear button wipes Input, expression, and result
+
+TODO - User Story: I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
+        -Order of operations handler (library for this?)
+
+TODO - Style UI
+
+*/
